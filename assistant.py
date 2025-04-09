e0a095f6d3f07fcffd04371657580952c24ed7c0
PetteriMoilanen <115905059+PetteriMoilanen@users.noreply.github.com>
2025-04-09 16:33:50 +0000

    added assistant.py and prompts.py

---
+++ assistant.py
@@ -0,0 +1,464 @@
+import os, getpass
+from typing import List
+from typing_extensions import TypedDict
+from pydantic import BaseModel, Field
+from langchain_openai import ChatOpenAI
+from langgraph.graph import START, END, StateGraph
+from langgraph.checkpoint.memory import MemorySaver
+from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
+import operator
+from typing import  Annotated
+from langgraph.graph import MessagesState
+from langchain_community.document_loaders import WikipediaLoader
+from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain_core.messages import get_buffer_string
+from langgraph.constants import Send
+from dotenv import load_dotenv
+
+import prompts # the prompts are in a separate file
+
+def _set_env_variables():
+    all_vars_set = True
+    for var in ["OPENAI_API_KEY", "GOOGLE_API_KEY", "LANGCHAIN_API_KEY", "TAVILY_API_KEY"]:
+        if not os.environ.get(var):
+            all_vars_set = False
+    if not all_vars_set:
+        load_dotenv()
+    for var in ["OPENAI_API_KEY", "GOOGLE_API_KEY", "LANGCHAIN_API_KEY", "TAVILY_API_KEY"]:
+        if not os.environ.get(var):
+            raise ValueError(f"Environment variable {var} not set. Please set it in your environment or .env file.")
+
+_set_env_variables()
+
+def _get_llm():
+    llm = ChatOpenAI(model="gpt-4o", temperature=0)
+    return llm
+
+llm = _get_llm()
+
+
+class Analyst(BaseModel):
+    affiliation: str = Field(
+        description="Primary affiliation of the analyst.",
+    )
+    name: str = Field(
+        description="Name of the analyst."
+    )
+    role: str = Field(
+        description="Role of the analyst in the context of the topic.",
+    )
+    description: str = Field(
+        description="Description of the analyst focus, concerns, and motives.",
+    )
+    @property
+    def persona(self) -> str:
+        return f"Name: {self.name}\nRole: {self.role}\nAffiliation: {self.affiliation}\nDescription: {self.description}\n"
+
+class Perspectives(BaseModel):
+    analysts: List[Analyst] = Field(
+        description="Comprehensive list of analysts with their roles and affiliations.",
+    )
+
+class GenerateAnalystsState(TypedDict):
+    topic: str # Research topic
+    max_analysts: int # Number of analysts
+    human_analyst_feedback: str # Human feedback
+    analysts: List[Analyst] # Analyst asking questions
+
+
+analyst_instructions = prompts.analyst_instruction
+
+
+def create_analysts(state: GenerateAnalystsState):
+
+    """ Create analysts """
+
+    topic=state['topic']
+    max_analysts=state['max_analysts']
+    human_analyst_feedback=state.get('human_analyst_feedback', '')
+
+    # Enforce structured output
+    structured_llm = llm.with_structured_output(Perspectives)
+
+    # System message
+    system_message = analyst_instructions.format(topic=topic,
+                                                            human_analyst_feedback=human_analyst_feedback,
+                                                            max_analysts=max_analysts)
+
+    # Generate question
+    analysts = structured_llm.invoke([SystemMessage(content=system_message)]+[HumanMessage(content="Generate the set of analysts.")])
+
+    # Write the list of analysis to state
+    return {"analysts": analysts.analysts}
+
+def human_feedback(state: GenerateAnalystsState):
+    """ No-op node that should be interrupted on """
+    pass
+
+def should_continue(state: GenerateAnalystsState):
+    """ Return the next node to execute """
+
+    # Check if human feedback
+    human_analyst_feedback=state.get('human_analyst_feedback', None)
+    if human_analyst_feedback:
+        return "create_analysts"
+
+    # Otherwise end
+    return END
+
+class InterviewState(MessagesState):
+    max_num_turns: int # Number turns of conversation
+    context: Annotated[list, operator.add] # Source docs
+    analyst: Analyst # Analyst asking questions
+    interview: str # Interview transcript
+    sections: list # Final key we duplicate in outer state for Send() API
+
+class SearchQuery(BaseModel):
+    search_query: str = Field(None, description="Search query for retrieval.")
+
+question_instructions = prompts.question_instructions
+
+def generate_question(state: InterviewState, llm):
+    """ Node to generate a question """
+
+    # Get state
+    analyst = state["analyst"]
+    messages = state["messages"]
+
+    # Generate question
+    system_message = question_instructions.format(goals=analyst.persona)
+    question = llm.invoke([SystemMessage(content=system_message)]+messages)
+
+    # Write messages to state
+    return {"messages": [question]}
+
+# Web search tool
+tavily_search = TavilySearchResults(max_results=3)
+# Wikipedia search tool
+
+# Search query writing
+
+search_instructions = SystemMessage(content=prompts.search_instructions)
+
+def search_web(state: InterviewState, llm):
+
+    """ Retrieve docs from web search """
+
+    # Search query
+    structured_llm = llm.with_structured_output(SearchQuery)
+    search_query = structured_llm.invoke([search_instructions]+state['messages'])
+
+    # Search
+    search_docs = tavily_search.invoke(search_query.search_query)
+
+     # Format
+    formatted_search_docs = "\n\n---\n\n".join(
+        [
+            f'<Document href="{doc["url"]}"/>\n{doc["content"]}\n</Document>'
+            for doc in search_docs
+        ]
+    )
+
+    return {"context": [formatted_search_docs]}
+
+def search_wikipedia(state: InterviewState, llm):
+
+    """ Retrieve docs from wikipedia """
+
+    # Search query
+    structured_llm = llm.with_structured_output(SearchQuery)
+    search_query = structured_llm.invoke([search_instructions]+state['messages'])
+
+    # Search
+    search_docs = WikipediaLoader(query=search_query.search_query,
+                                  load_max_docs=2).load()
+
+     # Format
+    formatted_search_docs = "\n\n---\n\n".join(
+        [
+            f'<Document source="{doc.metadata["source"]}" page="{doc.metadata.get("page", "")}"/>\n{doc.page_content}\n</Document>'
+            for doc in search_docs
+        ]
+    )
+
+    return {"context": [formatted_search_docs]}
+
+answer_instructions = prompts.answer_instructions
+def generate_answer(state: InterviewState, llm):
+
+    """ Node to answer a question """
+
+    # Get state
+    analyst = state["analyst"]
+    messages = state["messages"]
+    context = state["context"]
+
+    # Answer question
+    system_message = answer_instructions.format(goals=analyst.persona, context=context)
+    answer = llm.invoke([SystemMessage(content=system_message)]+messages)
+
+    # Name the message as coming from the expert
+    answer.name = "expert"
+
+    # Append it to state
+    return {"messages": [answer]}
+
+def save_interview(state: InterviewState):
+
+    """ Save interviews """
+
+    # Get messages
+    messages = state["messages"]
+
+    # Convert interview to a string
+    interview = get_buffer_string(messages)
+
+    # Save to interviews key
+    return {"interview": interview}
+
+def route_messages(state: InterviewState,
+                   name: str = "expert"):
+
+    """ Route between question and answer """
+
+    # Get messages
+    messages = state["messages"]
+    max_num_turns = state.get('max_num_turns',2)
+
+    # Check the number of expert answers
+    num_responses = len(
+        [m for m in messages if isinstance(m, AIMessage) and m.name == name]
+    )
+
+    # End if expert has answered more than the max turns
+    if num_responses >= max_num_turns:
+        return 'save_interview'
+
+    # This router is run after each question - answer pair
+    # Get the last question asked to check if it signals the end of discussion
+    last_question = messages[-2]
+
+    if "Thank you so much for your help" in last_question.content:
+        return 'save_interview'
+    return "ask_question"
+
+section_writer_instructions = prompts.section_writer_instructions
+def write_section(state: InterviewState, llm):
+
+    """ Node to answer a question """
+
+    # Get state
+    interview = state["interview"]
+    context = state["context"]
+    analyst = state["analyst"]
+
+    # Write section using either the gathered source docs from interview (context) or the interview itself (interview)
+    system_message = section_writer_instructions.format(focus=analyst.description)
+    section = llm.invoke([SystemMessage(content=system_message)]+[HumanMessage(content=f"Use this source to write your section: {context}")])
+
+    # Append it to state
+    return {"sections": [section.content]}
+
+# Add nodes and edges
+interview_builder = StateGraph(InterviewState)
+interview_builder.add_node("ask_question", generate_question)
+interview_builder.add_node("search_web", search_web)
+interview_builder.add_node("search_wikipedia", search_wikipedia)
+interview_builder.add_node("answer_question", generate_answer)
+interview_builder.add_node("save_interview", save_interview)
+interview_builder.add_node("write_section", write_section)
+
+# Flow
+interview_builder.add_edge(START, "ask_question")
+interview_builder.add_edge("ask_question", "search_web")
+interview_builder.add_edge("ask_question", "search_wikipedia")
+interview_builder.add_edge("search_web", "answer_question")
+interview_builder.add_edge("search_wikipedia", "answer_question")
+interview_builder.add_conditional_edges("answer_question", route_messages,['ask_question','save_interview'])
+interview_builder.add_edge("save_interview", "write_section")
+interview_builder.add_edge("write_section", END)
+
+# Interview
+memory = MemorySaver()
+interview_graph = interview_builder.compile(checkpointer=memory).with_config(run_name="Conduct Interviews")
+
+
+class ResearchGraphState(TypedDict):
+    topic: str # Research topic
+    max_analysts: int # Number of analysts
+    human_analyst_feedback: str # Human feedback
+    analysts: List[Analyst] # Analyst asking questions
+    sections: Annotated[list, operator.add] # Send() API key
+    introduction: str # Introduction for the final report
+    content: str # Content for the final report
+    conclusion: str # Conclusion for the final report
+    final_report: str # Final report
+
+
+def initiate_all_interviews(state: ResearchGraphState):
+    """ This is the "map" step where we run each interview sub-graph using Send API """
+
+    # Check if human feedback
+    human_analyst_feedback=state.get('human_analyst_feedback')
+    if human_analyst_feedback:
+        # Return to create_analysts
+        return "create_analysts"
+
+    # Otherwise kick off interviews in parallel via Send() API
+    else:
+        topic = state["topic"]
+        return [Send("conduct_interview", {"analyst": analyst,
+                                           "messages": [HumanMessage(
+                                               content=f"So you said you were writing an article on {topic}?"
+                                           )
+                                                       ]}) for analyst in state["analysts"]]
+
+report_writer_instructions = prompts.report_writer_instructions
+
+def write_report(state: ResearchGraphState, llm):
+    # Full set of sections
+    sections = state["sections"]
+    topic = state["topic"]
+
+    # Concat all sections together
+    formatted_str_sections = "\n\n".join([f"{section}" for section in sections])
+
+    # Summarize the sections into a final report
+    system_message = report_writer_instructions.format(topic=topic, context=formatted_str_sections)
+    report = llm.invoke([SystemMessage(content=system_message)]+[HumanMessage(content=f"Write a report based upon these memos.")])
+    return {"content": report.content}
+
+intro_conclusion_instructions = """You are a technical writer finishing a report on {topic}
+
+You will be given all of the sections of the report.
+
+You job is to write a crisp and compelling introduction or conclusion section.
+
+The user will instruct you whether to write the introduction or conclusion.
+
+Include no pre-amble for either section.
+
+Target around 100 words, crisply previewing (for introduction) or recapping (for conclusion) all of the sections of the report.
+
+Use markdown formatting.
+
+For your introduction, create a compelling title and use the # header for the title.
+
+For your introduction, use ## Introduction as the section header.
+
+For your conclusion, use ## Conclusion as the section header.
+
+Here are the sections to reflect on for writing: {formatted_str_sections}"""
+
+def write_introduction(state: ResearchGraphState):
+    # Full set of sections
+    sections = state["sections"]
+    topic = state["topic"]
+
+    # Concat all sections together
+    formatted_str_sections = "\n\n".join([f"{section}" for section in sections])
+
+    # Summarize the sections into a final report
+
+    instructions = intro_conclusion_instructions.format(topic=topic, formatted_str_sections=formatted_str_sections)
+    intro = llm.invoke([instructions]+[HumanMessage(content=f"Write the report introduction")])
+    return {"introduction": intro.content}
+
+def write_conclusion(state: ResearchGraphState):
+    # Full set of sections
+    sections = state["sections"]
+    topic = state["topic"]
+
+    # Concat all sections together
+    formatted_str_sections = "\n\n".join([f"{section}" for section in sections])
+
+    # Summarize the sections into a final report
+
+    instructions = intro_conclusion_instructions.format(topic=topic, formatted_str_sections=formatted_str_sections)
+    conclusion = llm.invoke([instructions]+[HumanMessage(content=f"Write the report conclusion")])
+    return {"conclusion": conclusion.content}
+
+def finalize_report(state: ResearchGraphState):
+    """ The is the "reduce" step where we gather all the sections, combine them, and reflect on them to write the intro/conclusion """
+    # Save full final report
+    content = state["content"]
+    if content.startswith("## Insights"):
+        content = content.strip("## Insights")
+    if "## Sources" in content:
+        try:
+            content, sources = content.split("\n## Sources\n")
+        except:
+            sources = None
+    else:
+        sources = None
+
+    final_report = state["introduction"] + "\n\n---\n\n" + content + "\n\n---\n\n" + state["conclusion"]
+    if sources is not None:
+        final_report += "\n\n## Sources\n" + sources
+    return {"final_report": final_report}
+
+# Add nodes and edges
+builder = StateGraph(ResearchGraphState)
+builder.add_node("create_analysts", create_analysts)
+builder.add_node("human_feedback", human_feedback)
+builder.add_node("conduct_interview", interview_builder.compile())
+builder.add_node("write_report",write_report)
+builder.add_node("write_introduction",write_introduction)
+builder.add_node("write_conclusion",write_conclusion)
+builder.add_node("finalize_report",finalize_report)
+
+# Logic
+builder.add_edge(START, "create_analysts")
+builder.add_edge("create_analysts", "human_feedback")
+builder.add_conditional_edges("human_feedback", initiate_all_interviews, ["create_analysts", "conduct_interview"])
+builder.add_edge("conduct_interview", "write_report")
+builder.add_edge("conduct_interview", "write_introduction")
+builder.add_edge("conduct_interview", "write_conclusion")
+builder.add_edge(["write_conclusion", "write_report", "write_introduction"], "finalize_report")
+builder.add_edge("finalize_report", END)
+
+# Compile
+memory = MemorySaver()
+graph = builder.compile(interrupt_before=['human_feedback'], checkpointer=memory)
+#display(Image(graph.get_graph(xray=1).draw_mermaid_png()))
+
+
+max_analysts = 3
+topic = "The history and development of LLMs"
+thread = {"configurable": {"thread_id": "1"}}
+#Run the graph until the first interruption
+for event in graph.stream({"topic":topic,
+                           "max_analysts":max_analysts},
+                          thread,
+                          stream_mode="values"):
+
+    analysts = event.get('analysts', '')
+    if analysts:
+        for analyst in analysts:
+            print(f"Name: {analyst.name}")
+            print(f"Affiliation: {analyst.affiliation}")
+            print(f"Role: {analyst.role}")
+            print(f"Description: {analyst.description}")
+            print("-" * 50)
+
+
+# We now update the state as if we are the human_feedback node
+graph.update_state(thread, {"human_analyst_feedback":
+                                "Add in a writer of an AI journal."}, as_node="human_feedback")
+# Confirm we are happy
+graph.update_state(thread, {"human_analyst_feedback":
+                            None}, as_node="human_feedback")
+# Continue
+for event in graph.stream(None, thread, stream_mode="updates"):
+    print("--Node--")
+    node_name = next(iter(event.keys()))
+    print(node_name)
+
+
+from IPython.display import Markdown
+final_state = graph.get_state(thread)
+report = final_state.values.get('final_report')
+print(Markdown(report))
+
+
+
